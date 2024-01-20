a6f301a2e71312f11829918855f38e088f087b8f
Pete Clark <peterclark1996@gmail.com>
2024-01-20 22:02:13 +0000

    Add game phases

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -7,8 +7,17 @@ using Random = UnityEngine.Random;
 public class GameControl : MonoBehaviour
 {
     public GameObject monkeyPrefab;
+    public GameObject backgroundObject;
+
+    public GamePhase currentPhase = GamePhase.MONKEY_MOVEMENT;
+    public FloorName currentFloor = Enum.GetValues(typeof(FloorName)).Cast<FloorName>().ToList().First();
+
+    private const float _travelTimeBetweenFloors = 1.5f;
 
     private readonly List<MonkeyObject> _monkeys = new();
+    private readonly List<FloorName> _allFloors = Enum.GetValues(typeof(FloorName)).Cast<FloorName>().ToList();
+    private FloorName? _targetFloor;
+    private float? _floorChangedTime;
 
     public int CountMonkeysOnLift() => _monkeys.Count(monkey =>
     {
@@ -16,7 +25,13 @@ public class GameControl : MonoBehaviour
         return monkeyX < Constants.Instance.LiftMaxRightPosition && monkeyX > Constants.Instance.LiftMaxLeftPosition;
     });
 
-    public bool AreMonkeysMoving() => _monkeys.Any(monkey => monkey.isMoving());
+    public void OnMonkeyFinishedMoving()
+    {
+        var areAnyMonkeysMoving = _monkeys.Any(monkey => monkey.isMoving());
+        if (areAnyMonkeysMoving) return;
+
+        currentPhase = GamePhase.PLAYER_INPUT;
+    }
 
     public static GameControl Instance;
 
@@ -24,10 +39,47 @@ public class GameControl : MonoBehaviour
     {
         Instance = gameObject.GetComponent<GameControl>();
     }
-    
+
     private void Start()
     {
         SpawnMonkey();
+    }
+
+    private void Update()
+    {
+        // Start Temp: handles user input, goes to a random floor
+        if (Input.GetButtonDown("Jump") && currentPhase == GamePhase.PLAYER_INPUT)
+        {
+            var allValidFloors = _allFloors.Where(floor => floor != currentFloor).ToList();
+            StartMovingTo(allValidFloors[Random.Range(0, allValidFloors.Count)]);
+        }
+        // End Temp
+
+        if (_targetFloor == null || !_floorChangedTime.HasValue) return;
+
+        var startIndex = _allFloors.FindIndex(floor => floor == currentFloor);
+        var targetIndex = _allFloors.FindIndex(floor => floor == _targetFloor);
+        var startPos = new Vector3(0, -startIndex * 10, 0);
+        var targetPos = new Vector3(0, -targetIndex * 10, 0);
+
+        if (Vector3.Distance(backgroundObject.transform.position, targetPos) < Mathf.Epsilon)
+        {
+            currentFloor = _targetFloor.Value;
+            _targetFloor = null;
+            _floorChangedTime = null;
+
+            currentPhase = GamePhase.MONKEY_MOVEMENT;
+            SpawnMonkey();
+
+            return;
+        }
+
+        var timePassed = Time.time - _floorChangedTime.Value;
+        var totalTravelDuration = Math.Abs(startIndex - targetIndex) * _travelTimeBetweenFloors;
+        var movementProgress = timePassed / totalTravelDuration;
+        var movementProgressSmoothed = Mathf.SmoothStep(0, 1, movementProgress);
+
+        backgroundObject.transform.position = Vector3.Lerp(startPos, targetPos, movementProgressSmoothed);
     }
 
     private void SpawnMonkey()
@@ -36,8 +88,16 @@ public class GameControl : MonoBehaviour
         var newMonkey = Instantiate(monkeyPrefab, spawnPos, new Quaternion());
         var monkeyObject = newMonkey.GetComponent<MonkeyObject>();
         _monkeys.Add(monkeyObject);
-        var allFloors = Enum.GetValues(typeof(Floor));
-        var desiredFloor = (Floor) allFloors.GetValue(Random.Range(0, allFloors.Length));
+        var desiredFloor = _allFloors[Random.Range(0, _allFloors.Count)];
         monkeyObject.Init(desiredFloor);
+    }
+
+    public void StartMovingTo(FloorName targetFloorName)
+    {
+        if (targetFloorName == currentFloor) return;
+
+        currentPhase = GamePhase.LIFT_MOVEMENT;
+        _targetFloor = targetFloorName;
+        _floorChangedTime = Time.time;
     }
 }
