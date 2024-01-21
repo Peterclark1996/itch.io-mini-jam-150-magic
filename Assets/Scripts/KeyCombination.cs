daf23056909bd822bedcc9b9f9c187ebfffe4f72
Thomas Heath <thomasheathtgh@gmail.com>
2024-01-21 15:39:47 +0000

    Added floor combination generator and a display for them

--- Assets/Scripts/KeyCombination.cs
+++ Assets/Scripts/KeyCombination.cs
@@ -2,9 +2,17 @@ using static Util;
 
 public struct KeyCombination
 {
-    public FloorName FloorName;
-    public Key Key1;
-    public Key Key2;
-    public Key Key3;
-    public Key Key4;
+    public FloorName FloorName { get; }
+    public Key Key1 { get; }
+    public Key Key2 { get; }
+    public Key Key3 { get; }
+    public Key Key4 { get; }
+
+    public KeyCombination(FloorName floorName, Key key1, Key key2, Key key3, Key key4) {
+        FloorName = floorName;
+        Key1 = key1;
+        Key2 = key2;
+        Key3 = key3;
+        Key4 = key4;
+    }
 }
--- Assets/Scripts/Spell.cs
+++ Assets/Scripts/Spell.cs
@@ -10,7 +10,11 @@ public class Spell : MonoBehaviour
 {
     [SerializeField]
     SpriteRenderer upArrow, downArrow, leftArrow, rightArrow;
+
+    [SerializeField]
+    Transform arrowPrefab;
     
+    List<KeyCombination> keyCombinations = new List<KeyCombination>();
     Dictionary<int, FloorName> floors = new Dictionary<int, FloorName>();
     List<Key> lastFourKeysPressed = new List<Key>();
 
@@ -20,12 +24,45 @@ public class Spell : MonoBehaviour
     void Awake()
     {
         List<FloorName> allFloors = Enum.GetValues(typeof(FloorName)).Cast<FloorName>().ToList();
+        float floorOffset = 0f;
+        Vector3 scale = new Vector3(0.25f, 0.25f, 1f);
         foreach (var floor in allFloors) {
+            KeyCombination keyCombination = new KeyCombination(
+                floor,
+                (Key)Random.Range(0, 4),
+                (Key)Random.Range(0, 4),
+                (Key)Random.Range(0, 4),
+                (Key)Random.Range(0, 4));
+            keyCombinations.Add(keyCombination);
+
+            Transform arrow = Instantiate(arrowPrefab);
+            arrow.SetParent(transform, false);
+            arrow.localScale = scale;
+            arrow.localPosition = new Vector3(0f, 1.5f + floorOffset, 0);
+            RotateArrow(keyCombination.Key1, arrow);
+            arrow = Instantiate(arrowPrefab);
+            arrow.SetParent(transform, false);
+            arrow.localScale = scale;
+            arrow.localPosition = new Vector3(0.8f, 1.5f + floorOffset, 0);
+            RotateArrow(keyCombination.Key2, arrow);
+            arrow = Instantiate(arrowPrefab);
+            arrow.SetParent(transform, false);
+            arrow.localScale = scale;
+            arrow.localPosition = new Vector3(1.6f, 1.5f + floorOffset, 0);
+            RotateArrow(keyCombination.Key3, arrow);
+            arrow = Instantiate(arrowPrefab);
+            arrow.SetParent(transform, false);
+            arrow.localScale = scale;
+            arrow.localPosition = new Vector3(2.4f, 1.5f + floorOffset, 0);
+            RotateArrow(keyCombination.Key4, arrow);
+
+            floorOffset++;
+
             var combination = new List<Key>() {
-                (Key)Random.Range(0, 4),
-                (Key)Random.Range(0, 4),
-                (Key)Random.Range(0, 4),
-                (Key)Random.Range(0, 4)
+                keyCombination.Key1,
+                keyCombination.Key2,
+                keyCombination.Key3,
+                keyCombination.Key4
             };
             floors.Add(GetKeySequenceHashCode(combination), floor);
         }
@@ -86,6 +123,22 @@ public class Spell : MonoBehaviour
             }
         }
         
+    }
+
+    void RotateArrow(Key key, Transform arrow) {
+        switch (key) {
+            case Key.Up:
+                break;
+            case Key.Down:
+                arrow.localEulerAngles = new Vector3(0, 0, 180);
+                break;
+            case Key.Left:
+                arrow.localEulerAngles = new Vector3(0, 0, 90);
+                break;
+            case Key.Right:
+                arrow.localEulerAngles = new Vector3(0, 0, 270);
+                break;
+        }
     }
 
     void ValidateCombination() {
